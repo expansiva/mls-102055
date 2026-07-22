b6378ab31769c59c64fc0d8520b19cc7980afe18
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-07-22 16:19:41 -0300

    Update project.ts

--- l2/project.ts
+++ l2/project.ts
@@ -1,4 +1,4 @@
-/// <mls fileReference="_102055_/l2/project.ts" enhancement="_blank" />
+/// <mls fileReference="_102055_/l2/project.ts" enhancement="_blank" /> 
 
 export const projectConfig = {
     masterFrontEnd: {
